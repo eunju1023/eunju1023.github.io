3d77b55af5e0c5b7e32bde4dc4ad7eb0aa34fe9b
eunju1023 <119823431+eunju1023@users.noreply.github.com>
2023-01-30 15:46:36 +0900

    Add files via upload

--- kakao/script/kakao.js
+++ kakao/script/kakao.js
@@ -7,7 +7,43 @@ $(function () {
     $(".lnb>li>ul").fadeOut();
     $(".lnb>li>ul").eq(i).fadeIn();
   });
+  
+  function night() {
+    $("a")
+      .css({ color: "#fff" });
+      $("p")
+      .css({ color: "#fff" });
+      $("h1")
+      .css({ color: "#fff" });
+    $("body")
+      .css({ backgroundColor: "#222" });
+      $(".download")
+      .css({ backgroundColor: "#292a2d" });
+      $(".foot span")
+      .css({ color: "#aaa" });
+      $(".btn .fa-solid")
+      .css({ color: "#aaa" });
+      $(".right ul li i")
+      .css({ color: "#fff" });
+      $(".lnb > li > ul")
+      .css({ backgroundColor: "#595959" });
+      $(".blank").css({backgroundColor: "#222"});
+}
 
+$(".fa-moon").click(function () {
+  night();
+      $(".fa-moon").removeClass("fa-moon").addClass("fa-solid fa-lightbulb");
+      $(".logo ul li img").remove("img");
+      $(".logo ul li").append("<img"+" src="+"images/kakao_logo_b.png"+" alt="+"logo_b"+"/>");
+      });
+
+
+// $(".fa-lightbulb").click(function () {
+//   $(".fa-lightbulb").removeClass("fa-lightbulb").addClass("fa-solid fa-moon");
+// });
+// $(".fa-lightbulb").css({ textShadow: "0 0 10px #ffff00" });
+
+  // e.preventDefault();
   function nextani() {
     $(".talkcontents").animate({ left: "-100%" }, 500, function () {
       $(".talkcontents>li").eq(0).appendTo(".talkcontents");
@@ -28,4 +64,5 @@ $(function () {
   $(".fa-circle-chevron-right").click(function () {
     nextani();
   });
+  
 });
